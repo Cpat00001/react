c9b2330cc3f0785b000587f62afc7e2ee5089065
Patryk <p171982@gmail.com>
2019-01-09 22:46:22 +0100

    modifications,some progress

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Contact from "./components/Contact";
 import Header from "./components/Header.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
@@ -8,7 +9,18 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <Contact />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="jdoes@gmail.com"
+            phone="555-555-5544"
+          />
+          <Contact
+            name="Sara William"
+            email="swill@gmail.com"
+            phone="333-333-333"
+          />
+        </div>
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,38 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Header = () => {
+const Header = props => {
+  const { branding } = props;
   return (
-    <div>
-      <h1>Contact Manager</h1>
-    </div>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py--3">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          {branding}
+        </a>
+        <div>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a href="/" className="nav-link">
+                Home
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
+};
+
+Header.defaultProps = {
+  branding: "My App"
+};
+Header.propTypes = {
+  branding: PropTypes.string.isRequired
+};
+
+const headingStyle = {
+  color: "pink",
+  fontSize: "50px"
 };
 
 export default Header;
