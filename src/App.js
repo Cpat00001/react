d786a9721cf979bf616bd1faf02ef6a5c2cddb0a
Patryk <p171982@gmail.com>
2019-01-10 21:46:17 +0100

    state and state handling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Contact from "./components/Contact";
+import Contacts from "./components/Contacts";
 import Header from "./components/Header.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -10,16 +10,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <div className="container">
-          <Contact
-            name="John Doe"
-            email="jdoes@gmail.com"
-            phone="555-555-5544"
-          />
-          <Contact
-            name="Sara William"
-            email="swill@gmail.com"
-            phone="333-333-333"
-          />
+          <Contacts />
         </div>
       </div>
     );
