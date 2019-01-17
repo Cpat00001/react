b7a568649ad1150a3ff994b654e68e232699fe0c
Patryk <p171982@gmail.com>
2019-01-17 18:19:38 +0100

    Updates

---
+++ precache-manifest.411dee4299b93c0831abab29091241fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "36b95de616a6fbc3c35c",
+    "url": "/static/js/main.36b95de6.chunk.js"
+  },
+  {
+    "revision": "6b9a0106f83f1b10d6dc",
+    "url": "/static/js/1.6b9a0106.chunk.js"
+  },
+  {
+    "revision": "36b95de616a6fbc3c35c",
+    "url": "/static/css/main.43d05066.chunk.css"
+  },
+  {
+    "revision": "6b9a0106f83f1b10d6dc",
+    "url": "/static/css/1.a2d66f78.chunk.css"
+  },
+  {
+    "revision": "c856cf44c3844c978d384b837aae498e",
+    "url": "/index.html"
+  }
+];
